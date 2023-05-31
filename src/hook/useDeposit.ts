cee81e30239e68bc6856d404ee9ecbf48a474acf
FairyFay404 <kanthila.23145@mail.kmutt.ac.th>
2023-05-31 19:00:33 +0700

    BigNumber from Fay

--- src/hook/useDeposit.ts
+++ src/hook/useDeposit.ts
@@ -1,18 +1,19 @@
 import { useContractWrite,usePrepareContractWrite } from "wagmi";
 import ShopABI from "../abis/Shop.json"
-import ethers, { BigNumber } from 'ethers'
+import ethers, { BigNumber, utils } from 'ethers'
+import { parseEther } from "ethers/lib/utils";
 
-const useDeposit = ( amount:BigNumber)=>{
-    const {config} = usePrepareContractWrite({
-        address: "0x31f31e8440C202A49CD77Cf2bD10e37fE01b1FA5",
-        abi: ShopABI,
-        functionName:"deposit",
-        args:[amount],
-        value: ethers.utils.parseEther.prototype((amount)),
-    });
-
-    const {data, isLoading, isError, write} = useContractWrite(config);
-    return {data, isLoading, isError, write};
+const useDeposit = ( amount:Number)=> {
+  const { config } = usePrepareContractWrite({
+    address: '0x31f31e8440C202A49CD77Cf2bD10e37fE01b1FA5',
+    abi: ShopABI,
+    functionName: 'deposit',
+    args:[amount],
+    // value: parseEther((amount)),
+    value: ethers.utils.parseEther.prototype(amount.toString()),
+  });
+  const {data, isLoading, isError, write} = useContractWrite(config);
+  return {data, isLoading, isError, write};
 
 };
 export default useDeposit;
