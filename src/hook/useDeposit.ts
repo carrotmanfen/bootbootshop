c1a6b4a63c204fca1d600660fcaa6d4c8f95b39c
FairyFay404 <kanthila.23145@mail.kmutt.ac.th>
2023-05-31 16:45:05 +0700

    BigNumber

--- src/hook/useDeposit.ts
+++ src/hook/useDeposit.ts
@@ -1,12 +1,14 @@
 import { useContractWrite,usePrepareContractWrite } from "wagmi";
 import ShopABI from "../abis/Shop.json"
+import ethers, { BigNumber } from 'ethers'
 
-const useDeposit = ( amount:number)=>{
+const useDeposit = ( amount:BigNumber)=>{
     const {config} = usePrepareContractWrite({
         address: "0x31f31e8440C202A49CD77Cf2bD10e37fE01b1FA5",
         abi: ShopABI,
         functionName:"deposit",
         args:[amount],
+        value: ethers.utils.parseEther.prototype((amount)),
     });
 
     const {data, isLoading, isError, write} = useContractWrite(config);
